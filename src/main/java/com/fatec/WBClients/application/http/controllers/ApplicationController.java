e7cdd4f2650d831286ca0f2db31ee6286a8eb6d0
henrique73 <henriqueerzingerd@gmail.com>
2021-04-28 10:24:16 -0300

    Updated Clients list, added statistics

--- src/main/java/com/fatec/WBClients/application/http/controllers/ApplicationController.java
+++ src/main/java/com/fatec/WBClients/application/http/controllers/ApplicationController.java
@@ -1,17 +1,53 @@
 package com.fatec.WBClients.application.http.controllers;
 
+import com.fatec.WBClients.application.services.UserAccountService;
+import com.fatec.WBClients.domain.UserAccount;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.OptionalDouble;
 
 @Controller
 public class ApplicationController implements WebMvcConfigurer {
 
+    @Autowired
+    private final UserAccountService profileService;
+
+    public ApplicationController(UserAccountService profileService) {
+        this.profileService = profileService;
+    }
+
     @RequestMapping("/")
-    public String homePage(Model model) {
+    public String homePage() {
         return "index";
+    }
+
+    @RequestMapping("/statistics")
+    public String statistics(Model model) {
+        List<UserAccount> MaleList = profileService.listAllGender("M");
+        List<UserAccount> FemaleList = profileService.listAllGender("F");
+        List<UserAccount> OtherList = profileService.listAllGender("O");
+        model.addAttribute("Male", (long) MaleList.size());
+        model.addAttribute("Female", (long) FemaleList.size());
+        model.addAttribute("Other", (long) OtherList.size());
+        List<UserAccount> AgeList = profileService.listAll();
+        model.addAttribute("AgeAverage",profileService.getAgesAverage(AgeList));
+        List<UserAccount> MaleAgeList = profileService.listAllGender("M");
+        model.addAttribute("MaleAgeAverage",profileService.getAgesAverage(MaleAgeList));
+        List<UserAccount> FemaleAgeList = profileService.listAllGender("F");
+        model.addAttribute("FemaleAgeAverage",profileService.getAgesAverage(FemaleAgeList));
+        List<UserAccount> OtherAgeList = profileService.listAllGender("O");
+        model.addAttribute("OtherAgeAverage",profileService.getAgesAverage(OtherAgeList));
+        return "statistics";
     }
 
 }
--- src/main/java/com/fatec/WBClients/application/services/UserAccountService.java
+++ src/main/java/com/fatec/WBClients/application/services/UserAccountService.java
@@ -2,9 +2,14 @@ package com.fatec.WBClients.application.services;
 
 import com.fatec.WBClients.application.repositories.UserAccountRepository;
 import com.fatec.WBClients.domain.UserAccount;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.print.attribute.UnmodifiableSetException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +27,45 @@ public class UserAccountService {
                 .stream()
                 .map(UserAccount::new)
                 .collect(Collectors.toList());
+    }
+
+    public HashSet listAge() {
+        List<UserAccount> list = repository.findAll()
+                .stream()
+                .map(UserAccount::new)
+                .collect(Collectors.toList());
+        HashSet newlist = new HashSet();
+        list.forEach((temp) ->{
+            newlist.add(temp.getBirthdate());
+        });
+        return newlist;
+    }
+
+    public int getAgesAverage(List<UserAccount> AgeList){
+        HashSet newlist = new HashSet();
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedString = today.format(formatter);
+
+
+        AgeList.forEach((temp) ->{
+            newlist.add((Period.between(LocalDate.parse(temp.getBirthdate(), formatter), LocalDate.parse(formattedString, formatter)).getYears()));
+        });
+
+        IntSummaryStatistics stats = newlist.stream()
+                .mapToInt((x) -> (int) x)
+                .summaryStatistics();
+
+
+        return ((int)stats.getAverage());
+    }
+
+    public List<UserAccount> listAllGender(String Gender) {
+        List<UserAccount> list = repository.findAll()
+                .stream()
+                .filter(userAccount -> (userAccount.getGender()).equals(Gender))
+                .collect(Collectors.toList());
+        return list;
     }
 
     public UserAccount findById(Long id) {
